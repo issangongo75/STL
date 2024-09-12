ad2d229a798550bcce6e0771d9adbe6aebcca3be
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-09-12 21:21:25 +0300

    Sequence Container

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,9 +1,136 @@
 #include<iostream>
+#include<array>
+#include<vector>
+#include<stack>
+#include<list>
+#include<iterator>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n------------------------------------------------\n"
+
+//#define STL_ARRAY
+//#define STL_VECTOR
+//#define Stack
+#define STL_LIST
+
+
+template<typename T>void vec_info(const std::vector<T>& vec);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hi STL!! " << endl;
+	//cout << "Hi STL!! " << endl;
+
+#ifdef STL_ARRAY
+
+	const int N = 5;
+	std::array<int, N> arr = { 3,5,8,13,21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+#endif // STL_ARRAY
+
+#ifdef STL_VECTOR
+
+	std::vector<int> vec = { 0,1,1,2,3,5,6,7,8, };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+	vec_info(vec);
+	vec.push_back(55);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << tab;
+	}
+	cout << endl;
+	vec_info(vec);
+	vec.reserve(15);
+	vec_info(vec);
+
+	//vec.shrink_to_fit();
+	vec_info(vec);
+	vec.resize(8);
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	//vec.resize(14);
+	for (int i : vec)cout << i << tab; cout << endl;
+	vec_info(vec);
+
+	vec.insert(vec.begin() + 4, { 1024,2048,3072,4096 });
+	for (int i : vec)cout << i << tab; cout << endl;
+
+	int index;
+	int count;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите количество добавляемого элементов: "; cin >> count;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+
+	//vec.insert(vec.begin() + index , value);
+	vec.insert(vec.begin() + index, count , value);
+	for (int i : vec)cout << i << tab; cout << endl;
+	vec_info(vec);
+
+
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите количество добавляемого элементов: "; cin >> count;
+	vec.erase(vec.begin() + index, vec.begin() + index + count);
+	for (int i : vec)cout << i << tab; cout << endl;
+#endif // STL_VECTOR
+
+#ifdef STL_LIST
+	std::list<int> list = { 3,5,8,13,21 };
+	//for (int i = 0; i < list.size(); i++)
+	{
+	//	cout << list[i] << tab;
+	}
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+		cout << *it << tab;
+	cout << endl;
+	cout << list.front() << endl;
+	cout << list.back() << endl;
+	list.push_front(2);
+	list.push_back(34);
+	list.emplace_front(1);
+	for (int i : list)cout << i << tab; cout << endl;
+
+
+	int index;
+	int value;
+	
+	do
+	{
+		cout << "Введите индекс добавляемого элемента: "; cin >> index;
+		if (index >= list.size())cout << "Error: Out of range" << endl;
+	} while (index >= list.size());
+	cout << "Введите индекс добавляемого элемента: "; cin >> value;
+	std::list<int>::iterator position = list.begin();
+	//for (int i = 0; i < index; i++)position++;
+	std::advance(position, index);
+	list.insert(position , value);
+	for (int i : list)cout << i << tab; cout << endl;
+
+#endif // STL_LIST
+
+
+}
+
+
+template<typename T>void vec_info(const std::vector<T>& vec)
+{
+	cout << "Size: " << vec.size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
 }
