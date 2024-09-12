ca30b6493851917cfc0e81841d92b7ebb2d62d0c
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-09-12 21:54:09 +0300

    AssociativeContainers

---
+++ AssociativeContainers/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n---------------------------------\n"
+
+/*
+map,set,multimap,multiset;
+*/
+
+#define STL_MAP
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef STL_MAP												
+	std::map<int, std::string> week =
+	{
+		std::pair<int , std::string>(0,"Sunday"),
+		std::pair<int , std::string>(1,"Monday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(2,"Tuesday"),
+		std::pair<int , std::string>(3,"Wednesday"),
+		{4,"Thursday"},
+		{5,"Friday"},
+		{6,"Saturday"},
+		{7,"Sunday"}
+	};
+
+	for (std::map<int, std::string>::iterator it = week.begin(); it != week.end(); ++it)
+	{
+		cout << it->first << tab << it->second << endl;
+	}
+#endif // STL_MAP
+
+}
